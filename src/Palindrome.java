7e1ba34d8132b9da6acc39b59a91df9868dbb496
aysenur.toprak <toprakaysenur96@gmail.com>
2022-09-25 01:53:17 +0300

    Denemeler

---
+++ src/Palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    static boolean Palindrome(int number){
+        int temp = number,reverseNumber = 0, lastDigit;
+        while (temp != 0){
+            lastDigit = temp % 10 ;
+            reverseNumber = (reverseNumber * 10) + lastDigit ;
+            temp /= 10 ;
+        }
+        if (number == reverseNumber) {
+            System.out.println(number + " is a palindrome number !");
+            return true;
+        } else {
+            System.out.println(number + " is not a palindrome number !");
+            return false;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = input.nextInt();
+        Palindrome(number);
+    }
+
+}
